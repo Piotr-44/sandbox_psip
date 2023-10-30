749d8b597c48f9984d0189f9cb7591083a8639b8
Piotr-44 <149382274+Piotr-44@users.noreply.github.com>
2023-10-30 14:46:00 +0100

    add read funcionalities

---
+++ dane.py
@@ -0,0 +1,7 @@
+users_list: list = [
+    {"nick":"muł", "name":"Piotrek", "posts":5},
+    {"nick":"kwadrat", "name":"Szymek", "posts":9},
+    {"nick":"geodeta naczelny", "name":"Oliver", "posts":123},
+    {"nick":"Król", "name":"Stanisław","posts":79},
+    {"nick":"kozak", "name":"Szymon","posts":199}
+]
---
+++ main.py
@@ -0,0 +1,11 @@
+from dane import users_list
+
+for user in users_list:
+    print(f'Twój znajomy {user["nick"]} dodał {user["posts"]}')
+    
+
+
+
+
+
+
