7aa52cc1a08c10e5e2a62decfce27a312ee2bbe6
Piotr-44 <149382274+Piotr-44@users.noreply.github.com>
2023-11-06 14:02:21 +0100

    chore:add_new_remove_function

--- dane.py
+++ dane.py
@@ -1,6 +1,6 @@
 users_list: list = [
     {"nick":"muł", "name":"Piotrek", "posts":5},
-    {"nick":"kwadrat", "name":"Szymek", "posts":9},
+    {"nick":"kwadrat", "name":"Szymon", "posts":9},
     {"nick":"geodeta naczelny", "name":"Oliver", "posts":123},
     {"nick":"Król", "name":"Stanisław","posts":79},
     {"nick":"kozak", "name":"Szymon","posts":199},
--- main.py
+++ main.py
@@ -12,9 +12,33 @@ def add_user_to(users_list:list)-> None:
     users_list.append({'name':name, 'posts': posts})
 
 
-add_user_to(users_list)
-add_user_to(users_list)
-add_user_to(users_list)
+def remove_user_from(users_list: list) -> None:
+    """
+    remove object from list
+    :param users_list: list - user list
+    :return: None
+    """
+    tmp_list = []
+    name = input ('podaj imie uzytkownika do usuniecia?')
+    for user in users_list:
+        if user["name"]== name:
+            tmp_list.append(user)
+
+    print('Znaleziono uzytkownikow:')
+    print('0:Usuń wszystkich znalezionych uzytkownikow')
+    for numerek, user_to_be_removed in enumerate(tmp_list):
+        print(f'{numerek+1}: {user_to_be_removed}')
+    numer = int(input(f'Wybierz numer użytkownika do usuniecia: '))
+    if numer == 0:
+        for user in tmp_list:
+            users_list.remove(user)
+    else:
+        users_list.remove(tmp_list[numer-1])
+
+remove_user_from(users_list)
+
+#add_user_to(users_list)
+#users_list.remove({"nick":"aaa", "name":"Agata","posts":279})
 
 
 for user in users_list:
