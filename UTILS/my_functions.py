03cfa6974b79aef042aed57589fa15923a27bb50
Piotr-44 <149382274+Piotr-44@users.noreply.github.com>
2023-12-04 14:13:28 +0100

    commit_pogoda

--- UTILS/my_functions.py
+++ UTILS/my_functions.py
@@ -169,3 +169,6 @@ def gui(users_list: list) -> None:
 
 
 #gui(users_list)
+def pogoda_z(miasto: str):
+    url = f"https://danepubliczne.imgw.pl/api/data/synop/station/{miasto}"
+    return  requests.get(url).json()
--- notatnik.py
+++ notatnik.py
@@ -1,69 +1,20 @@
-import re
 
-from bs4 import BeautifulSoup
 import requests
-import folium
+class User:
+    def __init__(self, miasto):
+        self.miasto = miasto
 
-#pobranie website'a
-
-nazwa_miejscowości = 'Biała_Podlaska'
-
-def get_coordinates_of(city:str)->list[float, float]:
-    # pobranie współrzędnych z treści strony internetowej
-
-    adres_URL = f'https://pl.wikipedia.org/wiki/{city}'
-
-    response = requests.get(url=adres_URL)
-    response_html = BeautifulSoup(response.text,'html.parser')
-
-    # . ponieważ odwołujemy się do klasy
-    # print(response_html_latitude[23:-7])
-    # latitude = re.sub("(\<).*?(\>)", repl='', string=response_html_latitude, count=0, flags=0)
-
-    response_html_latitude = response_html.select('.latitude')[1].text
-    response_html_latitude = float(response_html_latitude.replace(',','.'))
-    response_html_longitude = response_html.select('.longitude')[1].text
-    response_html_longitude = float(response_html_longitude.replace(',','.'))
-
-    return [response_html_latitude, response_html_longitude]
-
-#print(get_coordinates_of(nazwa_miejscowości))
+    def pogoda_z(self, miasto: str):
+        url = f"https://danepubliczne.imgw.pl/api/data/synop/station/{miasto}"
+        return requests.get(url).json()
 
 
-from dane import users_list
+npc_1 = User(miasto="warszawa")
+npc_2 = User(miasto="zamosc")
+print(npc_1.miasto)
+print(npc_2.miasto)
 
-#Zwrócić mapę z pinezką odnoszącą się do użytkownika podanego z klawiatury
-def get_map_one_user(user:str)->None:
-    city = get_coordinates_of(user["city"])
-    map = folium.Map(
-        location=city,
-        tiles="OpenStreetMap",
-        zoom_start=14
-    )
-    folium.Marker(
-        location=city,
-        popup=f'Tu rządzi {user["name"]},'
-              f'Liczba postów: {user["posts"]} '
-    ).add_to(map)
-    map.save(f'mapka_{user["name"]}.html')
+print(npc_1.pogoda_z(npc_1.miasto))
+print(npc_2.pogoda_z(npc_2.miasto))
 
-
-#Zwrócić mapę z wszystkimi użytkownika z danej listy (znajomych)
-
-
-#RYSOWANIE MAPY
-city = get_coordinates_of(city='Biała_Podlaska')
-def get_map_of(users: list[dict,dict])-> None:
-    map = folium.Map(location=[52.3,21.0], tiles="OpenStreetMap", zoom_start=7)
-
-    for user in users:
-        folium.Marker(
-        location=get_coordinates_of(city=user['city']),
-        popup=f'Użytkownik: {user["name"]} \n'
-              f'Liczba postów {user["posts"]}'
-        ).add_to(map)
-    map.save('mapka.html')
-
-from dane import users_list
-get_map_of(users_list)
-
+pogoda_z("warszawa")
